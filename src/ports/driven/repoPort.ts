55cfaa477c42231fca99151ce91ecd47b6f93a14
LaChevrenen <julien.andrade24@gmail.com>
2026-01-13 14:33:44 +0100

    refactor: remove Address-related classes and tests for cleanup

--- src/ports/driven/repoPort.ts
+++
@@ -1,7 +0,0 @@
-import { Address } from '../../domain/address';
-
-export interface AddressRepositoryPort {
-  findAll(): Promise<Address[]>;
-  findById(id: string): Promise<Address | null>;
-  save(address: Omit<Address, 'id'>): Promise<Address>;
-}
